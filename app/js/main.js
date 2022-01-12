43ca3f516d82c737614aa6b9bf1baf8e6fc43588
PavelLunin01 <luninpavel01@gmail.com>
2022-01-12 20:43:14 +0300

    added info

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+  $('.slider__list').slick({
+    prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/slider/arrow-left.svg" alt="arrow-left">',
+    nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/slider/arrow-right.svg" alt="arrow-right">',
+
+  });
+});
