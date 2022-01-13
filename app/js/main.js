583a55740cad9cac4ab87de76d6632c8b3431d32
PavelLunin01 <luninpavel01@gmail.com>
2022-01-14 00:18:46 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,15 @@ $(function () {
   $('.slider__list').slick({
     prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/slider/arrow-left.svg" alt="arrow-left">',
     nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/slider/arrow-right.svg" alt="arrow-right">',
+    responsive: [{
+      breakpoint: 481,
+      settings: {
+        arrows: false
+      }
+    }]
+  });
 
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('active');
   });
 });
