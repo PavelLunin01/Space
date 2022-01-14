3dfd6c420edbfc06dc2447173a9e255711240c62
PavelLunin01 <luninpavel01@gmail.com>
2022-01-14 05:43:11 +0300

    added menu and smooth scroll

--- app/js/main.js
+++ app/js/main.js
@@ -9,8 +9,48 @@ $(function () {
       }
     }]
   });
+  /* $(function () {
+
+  $('.menu__btn').on('click', function () {
+  $('.menu__list').toggleClass
+  
+  $('.menu__link').on('click', function () {
+  $('.menu__list').removeClass('menu__list--active');
+  $('.menu__btn').removeClass('menu__btn--active');
+  $('.body').removeClass('body--active');
+    });
+
+}); */
 
   $('.menu__btn').on('click', function () {
     $('.menu__list').toggleClass('active');
   });
+
+  $('.menu__link').on('click', function () {
+    $('.menu__list').removeClass('active');
+  });
+
+  const header__top = document.querySelector('.header__top');
+    window.onload = window.onscroll = () => {
+      if (window.pageYOffset > 50) {
+        header__top.classList.add('header__top-active');
+      }
+      else {
+        header__top.classList.remove('header__top-active');
+      }
+  }
+
+  $(".menu__list a").on("click", function (event) {
+ 
+    event.preventDefault();
+ 
+    var id  = $(this).attr('href'),
+ 
+    top = $(id).offset().top;
+ 
+    $('body,html').animate({
+      scrollTop: top - 65
+    }, 1500);
+  });
 });
+
